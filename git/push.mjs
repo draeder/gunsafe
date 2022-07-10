7609c4df8a6de9a318dc27b951ca36d56d6494c1
bresnow <bresnow@me.com>
2022-07-10 00:38:44 -0400

    Default Commit 7/10/2022, 12:38:43 | 1.0.0
    Signed-off-by: bresnow <bresnow@me.com>

--- git/push.mjs
+++ git/push.mjs
@@ -1,43 +1,37 @@
-import { $, question, YAML, chalk } from "zx";
-import { io } from "fsxx";
-import "zx/globals";
-import yargs from "yargs";
-$.verbose = false;
-let pkg = await io.json`package.json`;
-let args = yargs(process.argv.slice(3)).argv;
-let{ message, version}= args;
+import { $, question, YAML, chalk } from 'zx'
+import { io } from 'fsxx'
+import 'zx/globals'
+import yargs from 'yargs'
+$.verbose = false
+let pkg = await io.json`package.json`
+let args = yargs(process.argv.slice(3)).argv
+let { message, version } = args
 
 // Version prompt ... (if not provided current version is used)
 if (version === undefined) {
-  version = await question(
-    `${
-      chalk.green("Version? \n Current Version ") + chalk.cyan(pkg.data.version)
-    }: `
-  );
-  version === "" ? (version = pkg.data.version) : (version = version.trim());
+  version = await question(`${chalk.green('Version? \n Current Version ') + chalk.cyan(pkg.data.version)}: `)
+  version === '' ? (version = pkg.data.version) : (version = version.trim())
 }
 //PACKAGE>JSON MODIFY VERSION
-pkg.data.version = version;
-await pkg.save();
+pkg.data.version = version
+await pkg.save()
 
 //Commit Message (if not provided default message is used)
 if (message === undefined) {
-  message = await question(chalk.green("Message for commit : "));
-  if (message === "" || message.length < 2) {
-    message = `Default Commit ${new Date(Date.now())
-      .toLocaleString("en-US", { timeZone: "America/New_York" })
-      .slice(0, -3)}`;
+  message = await question(chalk.green('Message for commit : '))
+  if (message === '' || message.length < 2) {
+    message = `Default Commit ${new Date(Date.now()).toLocaleString('en-US', { timeZone: 'America/New_York' }).slice(0, -3)}`
   }
 }
 
 // Prettier and finalize
 
-await $`git status`;
+await $`git status`
 try {
-  await $`yarn prettier`;
+  await $`yarn prettier`
 } catch (error) {
-  console.log(chalk.red(error));
+  console.log(chalk.red(error))
 }
-await $`git add --all`;
-await $`git commit -s -m ${`${message} | ${version}`}`;
-await $`git push`;
+await $`git add --all`
+await $`git commit -s -m ${`${message} | ${version}`}`
+await $`git push`
--- tmp.mjs
+++ tmp.mjs
@@ -1,10 +1,8 @@
-import fs from 'fs/promises';
+import fs from 'fs/promises'
 
-
-    try {
-        const data = await fs.readFile(__dirname + '/pair.js', { encoding: 'base64' });
-        console.log(data);
-    } catch (err) {
-        console.log(err);
-    }
-
+try {
+  const data = await fs.readFile(__dirname + '/pair.js', { encoding: 'base64' })
+  console.log(data)
+} catch (err) {
+  console.log(err)
+}
